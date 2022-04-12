218f71026ace176dd4f7ef4c5d28808bdd1c3f70
Khemmie-Ray <atokemmy@gmail.com>
2022-04-12 14:14:34 -0700

    function nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,9 +13,11 @@ void print_alphabet_x10(void)
 	while (i < 10)
 	{
 		c = 'a';
+		while (c <= 'z')
 
-		while (c <= 'z')								{
-													_putchar(c);
+		{
+
+			_putchar(c);
 													c++;									}
 		_putchar('\n');
 		i++;									}
