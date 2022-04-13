2d14d8d07b15aa7716977ecdcf6833cca16dcc84
Khemmie-Ray <atokemmy@gmail.com>
2022-04-13 13:02:35 -0700

    Variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *   * main -prints numberz
+ *
+ *       * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
