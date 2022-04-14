022a271c3e0c6d0eed33601595d0c4d79db2de6f
Khemmie-Ray <atokemmy@gmail.com>
2022-04-14 13:48:30 -0700

    function nested loop

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,28 +6,28 @@
  *    * @b: second integer
  *     * @c: third integer
  *      * Return: largest number
-  */
+ */
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > c && b > a)
-	{
-		largest = b;
-	}
-	else if (c > b)
-	{
-		largest = c;
-	}
-	else
-	{
-		largest = b;
-	}
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c && b > a)
+{
+largest = b;
+}
+else if (c > b)
+{
+largest = c;
+}
+else
+{
+largest = b;
+}
 
-	return (largest);
+return (largest);
 }
