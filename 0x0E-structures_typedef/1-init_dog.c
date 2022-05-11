efb42ac8702d4cdbf214840c1c9e08425e63b96c
Khemmie-Ray <atokemmy@gmail.com>
2022-05-11 12:45:23 -0700

    Learning structures in c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  *  * init_dog - initializes a variable of type struct dog
@@ -9,11 +9,11 @@
  *      * @owner: owner to initialize
 */
 
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
